5a25cea6e6e6c4a7a6ab1801534bc9923f5145f6
Kwanele Nduku <kaykwanele226@gmail.com>
2026-09-12 02:44:27 +0200

    Register actionable options regression suite

--- run_regression_tests.py
+++ run_regression_tests.py
@@ -18,8 +18,9 @@ SUITES = [
     "phase6_accountability_tests.py",
     "phase7_regression_tests.py",
     "secondary_scope_regression_tests.py",
-    "joint_operations_regression_tests.py",
     "primary_production_regression_tests.py",
+    "joint_operations_regression_tests.py",
+    "actionable_options_regression_tests.py",
 ]
 
 
